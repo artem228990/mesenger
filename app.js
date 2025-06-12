6e03c3ae863a8adee827021a239e19ba0d0280a0
artem228990 <kosakartem76@gmail.com>
2025-06-12 21:22:36 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ const menuBtn = document.getElementById('menu-toggle');
 const sidebarMenu = document.getElementById('sidebar-menu');
 const chatSection = document.getElementById('chat-section');
 
-// Показуємо хедер та головний розділ після входу
+
 header.classList.remove('hidden');
 mainSection.classList.remove('hidden');
 
-// Кнопка меню відкриває/закриває панель
+
 menuBtn.onclick = () => {
   sidebarMenu.classList.toggle('visible');
 };
 
-// Показує чат тільки коли вибрано користувача
+
 function startChatWith(uid, nickname, avatarUrl) {
   currentChatUser = { uid, nickname, avatarUrl };
   chatWithSpan.textContent = nickname;
   chatSection.classList.add("visible");
-  sidebarMenu.classList.remove("visible"); // ховаємо меню
+  sidebarMenu.classList.remove("visible"); 
 
-  // ... решта логіки як раніше ...
+
 }
